177e31facc1b6faecd5c1fa63676cd4f1075bf17
JaxV9 <layanj.pro@gmail.com>
2025-05-23 10:10:30 +0200

    up

--- app/index.tsx
+++ app/index.tsx
@@ -1,30 +1,5 @@
-import { useAppSelector, useAppStore } from "@/store/hooks";
-import { useRef } from "react";
-import { Button, ColorValue, Text, View } from "react-native";
-import { Actions } from "@/store/actions/actions";
-import { counterValue } from "@/store/selector/counterSelector";
-import globalStyle from "@/styles/global";
+
 
 export default function Index() {
-
-  const value = useAppSelector(counterValue);
-  const actionsRef = useRef(new Actions());
-
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Button
-        onPress={() => actionsRef.current.incrementAction()}
-        title="Learn More"
-        color={"black"}
-        accessibilityLabel="Learn more about this purple button"
-      />
-      <Text>{value}</Text>
-    </View>
-  );
+  return <></>;
 }
